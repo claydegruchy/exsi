79aa78d6a4022226e87e9c7c73b98b37b189372d
POWER <clay.degruchy@gmail.com>
2021-04-01 12:43:21 +0200

    .

--- shutdownCheck.sh
+++ shutdownCheck.sh
@@ -1,9 +1,8 @@
-
-esxcli system syslog mark --message="[Auto-Shutdown Tool] Checking if ay VMs running for auto shutdown"
-if [[ $(esxcli vm process list | head -c1 | wc -c) == 1 ]]
+echo "[Auto-Shutdown Tool] Checking if any VMs are running for auto shutdown"
+esxcli system syslog mark --message="[Auto-Shutdown Tool] Checking if any VMs are running for auto shutdown"
+if [[ $(esxcli vm process list | head -c1 | wc -c) == 0 ]]
 then
   echo "Attempting shutdown"
   esxcli system syslog mark --message="[Auto-Shutdown Tool] No VMs running, shutting down"
-  # esxcli system shutdown poweroff -r="[Auto-Shutdown Tool] No VMs running, shutting down"
   poweroff
 fi
